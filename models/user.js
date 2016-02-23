e46115105eda147f4a339f05b17f870f34964d6f
Erich <eriforce@live.com>
2016-02-23 18:20:46 +0800

    added password hashing

--- models/user.js
+++ models/user.js
@@ -1,25 +1,52 @@
 var _ = require("lodash");
 var r = require("../utils/rethinkdb")();
+var bcrypt = require("co-bcryptjs");
 
 const TABLE_NAME = "users";
 const SCHEMA = ["userName", "password"];
 
 var table = r.table(TABLE_NAME);
 
-var User = function (properties) {
-    _.assign(this, properties);
+function* hashPassword(password) {
+    var salt = yield bcrypt.genSalt(8);
+    yield bcrypt.hash(password, salt);
 };
 
-User.findByUserName = function *(userName) {
+var User = function (properties) {
+    this.init();
+    _.assign(this, properties);
+    this.plainPassword = true;
+};
+
+User.findByUserName = function* (userName) {
     var user;
-    var result = yield table.filter({userName: userName});
-    if (result && result.length){
+    var result = yield table.filter({ userName: userName });
+    if (result && result.length) {
         user = result[0];
     }
     return user;
 };
 
+User.prototype.init = function () {
+    Object.defineProperty(this, 'password', {
+        get: function () {
+            return this._password;
+        },
+        set: function (password) {
+            this._password = password;
+        }
+    });
+};
+
+User.prototype.hashPassword = function* () {
+    if (this.plainPassword) {
+        this.plainPassword = false;
+        this.password = yield hashPassword(this.password);
+    }
+};
+
 User.prototype.save = function* () {
+    yield this.hashPassword();
     var model = _.pick(this, SCHEMA);
     var result;
     if (this.id) {
--- test/user.js
+++ test/user.js
@@ -5,13 +5,13 @@ require("co-mocha");
 describe("Test user model", function () {
     it("should create a user", function* () {
         var user = new User();
-        assert.equal(typeof user, "object");
+        assert.equal("object", typeof user);
     });
 
     it("should assign fields", function* () {
         var name = "erich";
         var user = new User({ userName: name });
-        assert.equal(user.userName, name);
+        assert.equal(name, user.userName);
     });
 
     it("should have id after being saved to db", function* () {
@@ -28,6 +28,23 @@ describe("Test user model", function () {
         var user = new User({ userName: name, password: password });
         yield user.save();
         var dbUser = yield User.findByUserName(name);
-        assert(dbUser.userName, name);
+        assert(name, dbUser.userName);
+    });
+
+    it("should have a hashed password", function* () {
+        var name = "erich";
+        var password = "pwd";
+        var user = new User({ userName: name, password: password });
+        yield user.save();
+        assert.notEqual(password, user.password);
+    });
+
+    it("should have same hashed password", function* () {
+        var name = "erich";
+        var password = "pwd";
+        var user = new User({ userName: name, password: password });
+        yield user.save();
+        var dbUser = yield User.findByUserName(name);
+        assert(user.userName, dbUser.userName);
     });
 });
