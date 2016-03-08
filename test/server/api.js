63abcbf9561e1a85e581b429abe6b9d054264372
Erich <eriforce@live.com>
2016-03-09 03:10:46 +0800

    updated /settings cases

--- test/server/api.js
+++ test/server/api.js
@@ -37,7 +37,7 @@ describe("Test private APIs", function() {
             assert(media);
             assert.deepEqual(Object.keys(media), Array.from(config.media));
             for (let ext of Object.keys(media)) {
-                assert(media[ext]);
+                assert.equal(media[ext], config.media.get(ext)[0]);
             }
         });
 
@@ -45,19 +45,23 @@ describe("Test private APIs", function() {
             let settings = {
                 media: {
                     ".mp4": constant.players.h5video,
-                    ".m4a": constant.players.none
+                    ".m4a": constant.players.none,
+                    ".wmv": constant.players.flash,  // mismatched player
+                    ".flv": "whatever",              // invalid player
+                    ".mpc": constant.players.h5audio // invalid extension
                 }
             };
+            let validSettings = { media: _.pick(settings.media, [".mp4", ".m4a"]) };
             yield agent.post(constant.urls.settings).send(settings).expect(200).end();
             let result = yield agent.get(constant.urls.settings).expect(200).end();
             let media = result.body.media;
             assert(media);
             assert.deepEqual(Object.keys(media), Array.from(config.media));
             for (let ext of Object.keys(media)) {
-                if (ext in settings.media)
-                    assert.equal(media[ext], settings.media[ext]);
+                if (ext in validSettings.media)
+                    assert.equal(media[ext], validSettings.media[ext]);
                 else
-                    assert(media[ext]);
+                    assert.equal(media[ext], config.media.get(ext)[0]);
             }
         });
     });
