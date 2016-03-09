4a93594c31899e0c07a4a052f88b02e129557a00
Erich <eriforce@live.com>
2016-03-10 00:45:45 +0800

    added mock fs

--- test/server/index.js
+++ test/server/index.js
@@ -3,11 +3,13 @@ require("co-mocha");
 let assert = require("assert");
 let util = require("util");
 let _ = require("lodash");
+let mockFs = require('mock-fs');
 let webApp = require("../../web-app");
 let User = require("../../models/user");
 let agentFactory = require("../../utils/agent-factory");
 let config = require("../../config");
 let constant = require("../../constant");
+let mock = require("../mock");
 
 describe("Test /index api", function () {
     let server = webApp.listen();
@@ -22,14 +24,17 @@ describe("Test /index api", function () {
         agent = agentFactory(server);
         let result = yield agent.post(constant.urls.signin).send({ userName: userName, password: password }).expect(200).end()
         agent.headers["Authorization"] = util.format("Bearer %s", result.body.token);
+
+        mockFs(mock.fs);
     });
     after(function* () {
         yield User.delete(user.userName);
+        mockFs.restore();
     });
-    
+
     it("should get 401 without token", function* () {
         yield agentFactory(server).get(constant.urls.index).expect(401).end();
     });
-    
-    
+
+
 });
