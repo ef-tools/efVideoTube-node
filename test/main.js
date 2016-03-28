fe99c261b5b726e302ec64dfc3f5ad961770f15e
Erich <eriforce@live.com>
2016-03-28 23:47:04 +0800

    made tests run on both databases

--- test/main.js
+++ test/main.js
@@ -1,13 +1,15 @@
 'use strict'
 let assert = require("assert");
 let config = require("../config");
+let constant = require("../constant");
 
 assert.equalCaseInsensitive = function (actual, expected) {
     assert.strictEqual(actual.toLowerCase(), expected.toLowerCase());
 };
 
-describe("Test on redis", function () {
-    config.db = "redis";
+let db = require("optimist").demand("config").argv.config || constant.db.redis;
+describe("Test on " + db, function () {
+    config.db = db;
     require("./models/user");
     require("./models/setting");
     require("./server/web-app");
